0cbcf8b634fbaf3ffce5fda019393fd0db24a1e5
7twin <32747235+7twin@users.noreply.github.com>
2018-05-30 04:16:05 +0200

    Added mid-text capslock detection
    Instead of checking the amount of uppercase overweighing the lowercase characters to "calm"/lowercase the entire message, the script now picks out all uppercased sentences and lowercases them only. This allows to detect mid-text capslocked sentences, instead of only detecting those, that have more uppercase than lowercase.

--- calmifier.user.js
+++ calmifier.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Devrant Calmifier
 // @namespace    https://devrant.com/
-// @version      0.1
+// @version      0.2
 // @description  Calm down rants, feed, comments and profiles
 // @author       7twin
 // @match        https://devrant.com/rants/*
@@ -14,11 +14,16 @@
 // @require      https://cdn.polyfill.io/v2/polyfill.min.js
 // ==/UserScript==
 
+/* config start */
+    // make capslocked sentences half transparent after lowercasing them
+    var highlight_changes = false;
+/* config end */
+
 (function() {
     $("h1.rantlist-content,.rantlist-title-text,.username-row+.rantlist-title").each(function(){
         var input = $(this).text();
         var lowercase = input.match(/[a-z]/gm); lowercase = ((lowercase) ? lowercase.length : 0);
-        var uppercase = input.match(/[A-Z]/gm); uppercase = ((uppercase) ? uppercase.length : 0);
+        var uppercase = input.match(/((\b[A-Z]{1,}\b)(?:\s*)){2,}/gm);
 
         // match only more than one occurence of exclamation or question marks
         // match only three or more full stops, because links get shortened with "..."
@@ -28,17 +33,21 @@
         input = input.replace(/([?!])\1+/gm,"$1");
         input = input.replace(/([.]){3,}/gm,"$1");
 
+        input = input.replace(/((\b[A-Z]{1,}\b)(?:\s*)){2,}/gm, function(m){
+            return ((highlight_changes === true) ? '<span style="color:rgba(255,255,255,0.65)">'+m.toLowerCase()+'</span>' : m.toLowerCase())
+        });
+
         // count all occurences in matched groups, to get how many have been replaced
-        punctuationMarks = ((punctuationMarks && punctuationMarks.length > 0) ? ($(this).text().match(/([?|!|.])/gm).length - punctuationMarks.length) : 0);
-        var total = punctuationMarks+lowercase+uppercase;
+        punctuationMarks = ((punctuationMarks && punctuationMarks.length > 0) ? punctuationMarks.reduce((total,currentItem) => total+currentItem.length, 0) : 0);
+        uppercase = ((uppercase && uppercase.length > 0) ? uppercase.reduce((total,currentItem) => total+currentItem.length, 0) : 0);
+
+        var total = $(this).text().length;
+        var changed = uppercase+punctuationMarks;
+        var diffPercent = Math.round((changed/total)*100);
 
         // only trigger if the text contains more uppercase than lowercase
-        if(lowercase <= uppercase){
-            // replace lowercase and punctuation marks
-            $(this).html((input.toLowerCase()+"<br><br><b>[calmed: " + ((uppercase === 0) ? 0 : Math.ceil(((uppercase+punctuationMarks)/total)*100)) + "%]</b>").replace(/\n/g, '<br />'));
-        }else if(punctuationMarks > 0){
-            // fallback to replacing punctuation marks
-            $(this).html((input+"<br><br><b>[calmed: " + Math.ceil((punctuationMarks/total)*100) + "%]</b>").replace(/\n/g, '<br />'));
+        if(diffPercent > 0){
+           $(this).html((input+"<br><br><b>[calmed: " + diffPercent + "%]</b>").replace(/\n/g, '<br />'));
         }
 
         // linkify links that got stripped
